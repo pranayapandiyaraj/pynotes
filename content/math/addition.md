45ff38790462bc2f3dc691afc070cd94c3f22631
pranayapandiyaraj <pranayapandiyaraj@gmail.com>
2026-01-04 13:30:46 +0530

    addition

---
+++ content/math/addition.md
@@ -0,0 +1,26 @@
+---
+title: Addition
+date: 2026-01-04
+author: Your Name
+cell_count: 2
+score: 0
+---
+
+```python
+a=3
+b=4
+c=a+b
+print(c)
+```
+
+    7
+    
+
+
+```python
+
+```
+
+
+---
+**Score: 0**
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'PRANAYA'  # use your name
+SITENAME        = 'Mypynotes' # use title
+GITHUB_USERNAME = 'https://github.com/pranayapandiyaraj' # use your username
 
 
 # Blogroll
@@ -12,8 +12,8 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
-    ("LinkedIn", "<linkedin handle>"),
+    ("GitHub", "https://github.com/pranayapandiyaraj/"),
+    ("LinkedIn", "https://www.linkedin.com/in/pranaya-pandiyaraj-13b4a0326/"),
 )
 
 ######## Advanced Settings (not recommended to to edited; take your own risk to touch the settings below) #############
